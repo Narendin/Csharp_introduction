dfda28a9f7d045c04c91278f6921462a41b45823
VladimirDolguev <avus93@mail.ru>
2021-06-15 21:08:08 +0300

    Урок 1
    Знакомство с Microsoft Visual Studio и языком C#

---
+++ lesson-1/lesson-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваше имя");
+            var name = Console.ReadLine();
+
+            DateTime dateTime = DateTime.Now;
+
+            Console.WriteLine($"Привет, {name}, сегодня {dateTime.ToShortDateString()}");
+        }
+    }
+}
