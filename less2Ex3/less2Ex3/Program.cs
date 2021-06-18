4526f740f2da3db6723cc631a802383b5d876f3b
VladimirDolguev <avus93@mail.ru>
2021-06-18 22:20:38 +0300

    Урок 2 Задание 3
    Домашнее задание

---
+++ less2Ex3/less2Ex3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace less2Ex3
+{
+    class Program
+    {
+        /*
+            Долгуев Владимир.
+            Определить, является ли введённое пользователем число чётным.
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            if (Int32.TryParse(Console.ReadLine(), out int number))
+            {
+                if ((number % 2) != 0)
+                {
+                    Console.WriteLine("Введенное число - нечетное");
+                }
+                else
+                {
+                    Console.WriteLine("Введенное число - четное");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введенное число содержит недопустимые символы либо содержит слишком большое количество символов");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
