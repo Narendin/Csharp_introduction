1b7b34117f05ce90ef26f3a3f64af220284cb3c6
VladimirDolguev <avus93@mail.ru>
2021-06-18 22:07:54 +0300

    Урок 2 Задание 2
    Домашнее задание

---
+++ less2Ex2/less2Ex2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace less2Ex2
+{
+    class Program
+    {
+        /*
+            Долгуев Владимир.
+            Запросить у пользователя порядковый номер текущего месяца и вывести его название.
+         */
+        enum Month
+        {
+            Январь = 1,
+            Февраль,
+            Март,
+            Апрель,
+            Май,
+            Июнь,
+            Июль,
+            Август,
+            Сентябрь,
+            Октябрь,
+            Ноябрь,
+            Декабрь
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер месяца");
+            if (Int32.TryParse(Console.ReadLine(), out int month))
+            {
+                if (month <= 12 && month > 0)
+                {
+                    Console.WriteLine((Month)month);
+                }
+                else
+                {
+                    Console.WriteLine("Месяца по введенному номеру не существует!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введенный номер содержит некорректные символы либо количество символов слишком велико");
+            }
+            Console.ReadLine();
+        }
+    }
+}
