e9af5ede3aa9889283f1bbdcacb5d4b0ca4caeef
VladimirDolguev <avus93@mail.ru>
2021-06-23 22:37:33 +0300

    Урок 3 Задание 3
    Домашнее задание

---
+++ lesson-3/less3Ex3/less3Ex3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace less3Ex3
+{
+    class Program
+    {
+        /*
+            Долгуев Владимир.
+            Написать программу, выводящую введённую пользователем 
+            строку в обратном порядке (olleH вместо Hello).
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            var line = Console.ReadLine();
+            var charArray = line.ToCharArray();
+            Array.Reverse(charArray);
+            Console.WriteLine(new string(charArray));
+            
+            Console.ReadKey();
+        }
+    }
+}
