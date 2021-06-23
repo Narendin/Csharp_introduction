c4735b5646c6cd50dd49f3975b1f9e7b1fec6308
VladimirDolguev <avus93@mail.ru>
2021-06-23 22:30:13 +0300

    Урок 3 Задание 2
    Домашнее задание

---
+++ lesson-3/less3Ex2/less3Ex2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace less3Ex2
+{
+    class Program
+    {
+        /*
+            Долгуев Владимир.
+            Написать программу «Телефонный справочник»: создать двумерный массив 5х2, 
+            хранящий список телефонных контактов: 
+            первый элемент хранит имя контакта, 
+            второй — номер телефона/email.
+         */
+        static void Main(string[] args)
+        {
+            var phonebook = new string[5, 2];
+
+            phonebook[0, 0] = "Иванов Иван";
+            phonebook[0, 1] = "8-965-568-62-98";
+
+            phonebook[1, 0] = "Сидоров Сидор";
+            phonebook[1, 1] = "8-965-568-62-98";
+
+            phonebook[2, 0] = "Петров Пётр";
+            phonebook[2, 1] = "8-965-568-62-98";
+
+            phonebook[3, 0] = "Михайлов Михаил";
+            phonebook[3, 1] = "8-965-568-62-98";
+
+            phonebook[4, 0] = "Николаев Николай";
+            phonebook[4, 1] = "8-965-568-62-98";
+
+            for (int i = 0; i < phonebook.GetLength(0); i++)
+            {
+                Console.WriteLine($"{phonebook[i, 0]}".PadRight(20) + $"{phonebook[i, 1]}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
