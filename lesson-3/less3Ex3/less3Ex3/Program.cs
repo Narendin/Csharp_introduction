97c1795b6b2613109d7e1df40a1f8d9967321a08
VladimirDolguev <avus93@mail.ru>
2021-06-24 20:34:47 +0300

    Урок 3 Задание 3
    Добавил второй метод вывода обратной строки

--- lesson-3/less3Ex3/less3Ex3/Program.cs
+++ lesson-3/less3Ex3/less3Ex3/Program.cs
@@ -17,6 +17,16 @@ namespace less3Ex3
         {
             Console.WriteLine("Введите строку");
             var line = Console.ReadLine();
+
+            for (int i = line.Length - 1; i >= 0; i--)
+            {
+                Console.Write(line[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // второй способ
             var charArray = line.ToCharArray();
             Array.Reverse(charArray);
             Console.WriteLine(new string(charArray));
