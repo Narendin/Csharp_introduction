71c3c529e4490c583e1c1f7e1c3f04cd01727368
VladimirDolguev <avus93@mail.ru>
2021-06-27 16:15:36 +0300

    Урок 4 Задание 2
    Написать программу, принимающую на вход строку — набор чисел, разделенных пробелом, и возвращающую число — сумму всех чисел в строке. Ввести данные с клавиатуры и вывести результат на экран.

---
+++ lesson-4/less4Ex2/less4Ex2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace less4Ex2
+{
+    class Program
+    {
+        /*
+            Долгуев Владимир.
+            Написать программу, принимающую на вход строку — набор чисел, 
+            разделенных пробелом, и возвращающую число — сумму всех чисел в строке. 
+            Ввести данные с клавиатуры и вывести результат на экран.
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа через пробел");
+            string numbers = Console.ReadLine();
+            var words = numbers.Split(' ');
+            int sum = 0;
+
+            foreach (var word in words)
+            {
+                sum += ParseLine(word);
+            }
+            Console.WriteLine($"Сумма введенных чисел равна {sum}");
+
+            Console.ReadKey();
+        }
+
+        static int ParseLine(string line)
+        {
+            bool isValid = int.TryParse(line, out int number);
+            if (!isValid) Exit();
+            return number;
+        }
+
+        private static void Exit()
+        {
+            Console.WriteLine("Строка содержит недопустимые символы");
+            Console.WriteLine("Для завершения работы приложения нажмите любую клавишу...");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
