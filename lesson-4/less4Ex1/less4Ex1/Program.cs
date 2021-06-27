4b3d77754699cc15c0a04b5194c5cfd9da91a8ea
VladimirDolguev <avus93@mail.ru>
2021-06-27 15:59:07 +0300

    Урок 4 Задание 1
    Написать метод GetFullName(string firstName, string lastName, string patronymic), принимающий на вход ФИО в разных аргументах и возвращающий объединённую строку с ФИО. Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.

---
+++ lesson-4/less4Ex1/less4Ex1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace less4Ex1
+{
+    class Program
+    {
+
+        /*
+            Долгуев Владимир.
+            Написать метод GetFullName(string firstName, string lastName, string patronymic), 
+            принимающий на вход ФИО в разных аргументах и возвращающий объединённую строку с ФИО. 
+            Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.
+         */
+
+        static void Main(string[] args)
+        {
+            var users = new Users[] {new Users() { firstName = "Иван", lastName = "Иванов", patronymic = "Иванович" },
+                                     new Users() { firstName = "Пётр", lastName = "Петров", patronymic = "Петрович" },
+                                     new Users() { firstName = "Семён", lastName = "Семёнов", patronymic = "Семёнович" },
+                                     new Users() { firstName = "Фёдор", lastName = "Фёдоров", patronymic = "Фёдорович" }
+                                    };
+
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.GetFullName());
+            }
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
