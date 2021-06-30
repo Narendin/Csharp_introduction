1476822d970042446a09720b07bb6d4690bdea91
VladimirDolguev <avus93@mail.ru>
2021-06-30 16:37:19 +0300

    Урок 5 Задание 3
    Ввести с клавиатуры произвольный набор чисел (0...255) и записать их в бинарный файл.

---
+++ lesson-5/less5Ex3/less5Ex3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace less5Ex3
+{
+    class Program
+    {
+        /*
+            Долгуев Владимир.
+            Ввести с клавиатуры произвольный набор чисел (0...255) и записать их в бинарный файл.
+         */
+        static void Main(string[] args)
+        {
+            string fileName = "bytes.bin";
+            while (true)
+            {
+                Console.WriteLine("Введите произвольный набор чисел (0...255) через пробел");
+                var line = Console.ReadLine();
+                byte[] array = GetByteArray(line, out bool correctLine);
+                if (correctLine)
+                {
+                    File.WriteAllBytes(fileName, array);
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Успешная запись в файл {fileName}");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Получение массива байтов из строки
+        /// </summary>
+        /// <param name="line"> Вводная строка</param>
+        /// <param name="correctLine"> Выходной флаг корректности строки</param>
+        /// <returns>Возвращает массив байтов из строки и флаг успешного преобразования</returns>
+        static byte[] GetByteArray(string line, out bool correctLine)
+        {
+            var numbers = line.Split(' ');
+            byte[] array = new byte[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                bool isValid = byte.TryParse(numbers[i], out byte currentNum);
+                if (isValid)
+                {
+                    array[i] = currentNum;
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("В строке присутствуют некорректные символы либо числа, не входящие в диапазон 0...255");
+                    correctLine = false;
+                    return array;
+                }
+            }
+            correctLine = true;
+            return array;   
+        }
+    }
+}
