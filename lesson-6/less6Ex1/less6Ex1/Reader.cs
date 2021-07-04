b390495fa49651f50811bfafe9adb22e3b7a155d
VladimirDolguev <avus93@mail.ru>
2021-07-04 21:03:09 +0300

    Урок 6 Задание 1
    Написать консольное приложение Task Manager, которое выводит на экран запущенные процессы и позволяет завершить указанный процесс. Предусмотреть возможность завершения процессов с помощью указания его ID или имени процесса. В качестве примера можно использовать консольные утилиты Windows tasklist и taskkill.

---
+++ lesson-6/less6Ex1/less6Ex1/Reader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace less6Ex1
+{
+    class Reader
+    {
+        /*
+         * Данный код честно скопипастчен с сайта overcoder.net
+         */
+
+        private static Thread inputThread;
+        private static AutoResetEvent getInput, gotInput;
+        private static string input;
+
+        static Reader()
+        {
+            getInput = new AutoResetEvent(false);
+            gotInput = new AutoResetEvent(false);
+            inputThread = new Thread(reader);
+            inputThread.IsBackground = true;
+            inputThread.Start();
+        }
+
+        private static void reader()
+        {
+            while (true)
+            {
+                getInput.WaitOne();
+                input = Console.ReadLine();
+                gotInput.Set();
+            }
+        }
+
+        public static bool TryReadLine(out string line, int timeOutMillisecs = Timeout.Infinite)
+        {
+            getInput.Set();
+            bool success = gotInput.WaitOne(timeOutMillisecs);
+            if (success)
+                line = input;
+            else
+                line = null;
+            return success;
+        }
+    }
+}
